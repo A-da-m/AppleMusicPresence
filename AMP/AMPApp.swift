70c5edcd28ac1b51f8e93b7bfbd86d66ef778e54
Adam <adam@atiktech.co>
2020-09-19 16:31:53 -0500

    Initial Commit

---
+++ AMP/AMPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMPApp.swift
+//  AMP
+//
+//  Created by Adam on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct AMPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
