d5c9120d96d496b6c1a2adc5198d344dcb7679f9
Adam <adam@atiktech.co>
2020-11-17 20:52:30 -0600

    :art: rewrote in swift and macOS 11 support

--- AMP/AMPApp.swift
+++ AMP/AMPApp.swift
@@ -6,12 +6,81 @@
 //
 
 import SwiftUI
+import SwordRPC
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  private var rpc = SwordRPC(appId: "594174908263694403")
+  var popover = NSPopover.init()
+  var statusBarItem: NSStatusItem?
+  
+  @objc
+  func updateTrack (notification: NSNotification) {
+    print(notification)
+    var presence = RichPresence()
+    let playerState = String(describing: notification.userInfo!["Player State"] ?? "Unknown")
+    presence.state = String(describing: notification.userInfo!["Artist"] ?? "Unknown")
+    if playerState == "Playing" {
+      presence.details = String(describing: notification.userInfo!["Name"] ?? "Unknown")
+      let endsAt =  Double(String(describing: notification.userInfo!["Total Time"] ?? "0"))! / 1000
+      presence.timestamps.start = Int(Date().timeIntervalSince1970)
+      presence.timestamps.end = Int((Date() + endsAt).timeIntervalSince1970)
+    } else {
+      presence.details = "Paused: \(String(describing: notification.userInfo!["Name"] ?? "Unknown"))"
+      presence.assets.smallImage = "pause"
+      presence.assets.smallText = "Paused"
+    }
+    presence.assets.largeImage = "am"
+    presence.assets.largeText = "Presence by Adаm#2917"
+    
+    rpc.setPresence(presence)
+  }
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    let contentView = ContentView()
+    
+    popover.behavior = .transient
+    popover.animates = false
+    popover.contentViewController = NSViewController()
+    popover.contentViewController?.view = NSHostingView(rootView: contentView)
+    statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    statusBarItem?.button?.title = "AMP"
+    statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+  
+    let notificationCenter = DistributedNotificationCenter.default()
+    rpc.onConnect { rp in
+      notificationCenter.addObserver(self, selector: #selector(self.updateTrack), name: NSNotification.Name(rawValue: "com.apple.Music.playerInfo"), object: nil)
+    }
+    rpc.onDisconnect { _, _, _ in
+      notificationCenter.removeObserver(self, name: NSNotification.Name(rawValue: "com.apple.Music.playerInfo"), object: nil)
+    }
+    rpc.connect()
+  }
+  
+  @objc func showPopover(_ sender: AnyObject?) {
+    if let button = statusBarItem?.button {
+      popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+    }
+  }
+  
+  @objc func closePopover(_ sender: AnyObject?) {
+    popover.performClose(sender)
+  }
+  @objc func togglePopover(_ sender: AnyObject?) {
+    if popover.isShown {
+      closePopover(sender)
+    } else {
+      showPopover(sender)
+    }
+  }
+}
 
 @main
 struct AMPApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDegelate
+  
+  var body: some Scene {
+    Settings {
+      Text("Empty")
     }
+  }
 }
--- AMP/ContentView.swift
+++ AMP/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    VStack{
+      Text("AMP")
+        .font(.title)
+        .fontWeight(.heavy)
+      Text("Beta 1.0.0")
+        .font(.subheadline)
+        .fontWeight(.light)
+      Button("Quit", action: {
+        NSApplication.shared.terminate(.none)
+      }).padding()
+    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
